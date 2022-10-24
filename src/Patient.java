a40945fc1141cf800db9c2950964d9d58688cc82
r-ferguson <92554295+r-ferguson@users.noreply.github.com>
2022-10-24 16:55:27 -0600

    Think this is correct but need to verify some things

--- src/Patient.java
+++ src/Patient.java
@@ -1,7 +1,16 @@
 import java.util.Date;
 
 public class Patient extends Person{
-    public Patient(String name, Date dob) {
+    //attributes
+    public String id;
+    public Date arrivalDate;
+    public Date dischargeDate;
+    public String status;
+
+    public Patient(String name, Date dob, String id, Date arrivalDate, String status) {
         super(name, dob);
+        this.arrivalDate = arrivalDate;
+        this.id = id;
+        this.status = status;
     }
 }
